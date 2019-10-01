611cac45a50e273c9d4175d3ed25559a8bb14d46
Gram <master_fess@mail.ru>
2019-10-01 14:25:00 +0200

    test pre-validation

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -40,3 +40,31 @@ def test_invalid_int(validator):
     v = validator(data=data)
     assert v.is_valid() is False
     assert v.errors
+
+
+@pytest.mark.parametrize('validator', prevalidators)
+def test_types_converting(validator):
+    data = {
+        'name': 'Max',
+        'mail': 'test@example.ru',
+        'count': '10',
+    }
+    v = validator(request=True, data=data)
+    assert v.is_valid() is True
+    assert not v.errors
+    assert 'count' in v.cleaned_data
+    assert v.cleaned_data['count'] == 10
+
+
+@pytest.mark.parametrize('validator', prevalidators)
+def test_explicit_keys(validator):
+    data = {
+        'name': 'Max',
+        'mail': 'test@example.ru',
+        'count': 10,
+        'junk': 'test',
+    }
+    v = validator(request=True, data=data)
+    assert v.is_valid() is True
+    assert not v.errors
+    assert 'junk' not in v.cleaned_data
